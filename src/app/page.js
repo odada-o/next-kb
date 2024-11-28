7790549def7d78707f84f38737c679ddec0fad71
odd:odd <eehd80yul@gmail.com>
2024-11-28 17:26:56 +0900

    section2

--- src/app/page.js
+++ src/app/page.js
@@ -3,10 +3,10 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
+    <div className="pb-96 flex flex-col gap-4">
       <section>
-        <div>
-         <div className="text-[17vw] font-black">thinK Great</div>
+        <div className="container">
+         <div className="text-[16vw] font-black">thinK Great</div>
          <div className="relative">
           <Image src="/images/intro.avif" alt="" width={1300} height={1300} className="w-full h-[500px] object-cover" />
           <div className="absolute left-5 bottom-10 flex flex-col gap-2">
@@ -18,13 +18,17 @@ export default function Home() {
       </section>
 
       <section>
-        <div>
+        <div className="container">
           <ul>
             <li>
-              <Link href="#">
-                <Image src="/images/media-1.avif" alt="" width={1300} height={1300} className="w-full aspect-video object-cover" />
-                <span className="text-md text-gray-500">소셜미디어</span>
-                <strong className="text-xl">소통과 화합의 현장! KG모빌리티 파트너스 데이</strong>
+              <Link href="#" className="flex flex-col gap-3">
+                <Image src="/images/media-3.avif" alt="" width={1300} height={1300} className="w-full aspect-video object-cover" />
+
+                <div className="flex flex-col">
+                  <span className="text-md text-gray-500">소셜미디어</span>
+                  <strong className="text-xl">소통과 화합의 현장! KG모빌리티 파트너스 데이</strong>
+                </div>
+
               </Link>
             </li>
           </ul>
